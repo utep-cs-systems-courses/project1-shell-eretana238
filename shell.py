2998a61d8cdc691594d46013defaed0648c4606d
eretana238 <eretana238@gmail.com>
2020-09-14 09:26:38 -0600

    simplified shell

--- shell.py
+++ shell.py
@@ -4,25 +4,36 @@ import os, sys, re
 
 pid = os.getpid()               # get and remember pid
 
-def execute_command(commands):
-    for dir in re.split(":", os.environ['PATH']): # try each directory in path
-        program = "%s/%s" % (dir, commands[0])
-        try:
-            os.execve(program, args, os.environ) # try to exec program
-        except FileNotFoundError:             # ...expected
-            pass                              # ...fail quietly 
-
+builtin_cmd = ['cd','exit']
 prompt_string = '$ '
 if 'PS1' in os.environ:
     prompt_string = os.environ['PS1']
+
+def pipe_process():
+    pass
+
+def exec_builtin(in_str):
+    if in_str not in builtin_cmd:
+        return 0
+    if in_str.startsWith('exit'):
+        sys.exit(0)
+    elif in_str.startsWith('cd'):
+        pass
+    return 0
+
+def exec_command(commands):
+    for dir in re.split(":", os.environ['PATH']): 
+        program = "%s/%s" % (dir, commands[0])
+        try:
+            os.execve(program, args, os.environ) 
+        except FileNotFoundError:
+            pass
 
 while True:
     user_input = input(prompt_string)
     if not user_input:
         continue
     args = re.split(' ', user_input)
-    if args[0] == 'exit':
-        sys.exit(0)
 
     rc = os.fork()
     
@@ -31,8 +42,13 @@ while True:
         sys.exit(1)
 
     elif rc == 0:                   # child
-        execute_command(args)
+        os.close(1)                 # redirect child's stdout
+        # os.open("p4-output.txt", os.O_CREAT | os.O_WRONLY);
+        # os.set_inheritable(1, True)
+
+        exec_command(args)
 
         sys.exit(1)          
     else:                           # parent (forked ok)
         childPidCode = os.wait()
+        os.write(1, ("Parent: Child %d terminated with exit code %d\n" % childPidCode).encode())
