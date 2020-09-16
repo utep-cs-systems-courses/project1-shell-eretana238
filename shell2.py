1152932f53805a740c6047b5f9f5a29caaee826b
eretana238 <eretana238@gmail.com>
2020-09-15 20:08:50 -0600

    finished 7 test cases | missing pipe and background

--- shell2.py
+++ shell2.py
@@ -11,43 +11,30 @@ def exec_builtins(command):
     if command[0] == 'exit':
         sys.exit(0)
     elif command[0] == 'cd': 
-        cwd = os.getcwd()
         if len(command) > 1:
             try: 
                 os.chdir(command[1])
             except FileNotFoundError: 
-                print('-bash: cd: %s: No such file or directory' % command[1])
+                print('-bash: cd: %s: No such directory' % command[1])
+            except NotADirectoryError:
+                print('-bash: cd: %s: No such directory' % command[1])
 
 # checks and executes builtin commands "cd" and "exit"
-def exec_command(command):
+def exec_command(commands):
     for dir in re.split(":", os.environ['PATH']): 
-        program = "%s/%s" % (dir, command[0])
+        program = "%s/%s" % (dir, commands[0])
         try:
-            os.execve(program, command, os.environ)
+            os.execve(program, commands, os.environ)
         except FileNotFoundError:
             pass
-        
-# parese redirect provided by os instructors
-def parse_redirect(user_input):
-    output_file = None
-    input_file = None
-    cmd = ''
- 
-    if '>' in user_input:
-        [cmd, output_file] = user_input.split('>',1)
-        output_file = output_file.strip()
- 
-    elif '<' in cmd:
-        [cmd, input_file] = cmd.split('<', 1)
-        input_file = input_file.strip()
-    
-    elif output_file != None and '<' in output_file:
-        [output_file, input_file] = output_file.split('<', 1)
-        
-        output_file = output_file.strip()
-        input_file = input_file.strip()
-        
-    return cmd.split(), output_file, input_file
+
+def redirect(args,n):
+    os.close(n)                 # redirect child's stdout
+    if n == 0:
+        os.open(args[-1], os.O_RDONLY);
+    else:
+        os.open(args[-1], os.O_CREAT | os.O_WRONLY);
+    os.set_inheritable(n, True)
 
 def run_process(args):
     rc = os.fork()
@@ -57,14 +44,44 @@ def run_process(args):
         sys.exit(1)
 
     elif rc == 0:                   # child
-        exec_command(args)
-        sys.exit(1)
+        if '>' in args:
+            redirect(args,1)
+            exec_command(args)
+            os.write(2, ("Child:    Error: Could not exec %s\n" % args[0]).encode())
+            sys.exit(1)
+        elif '<' in args:
+            redirect(args,0)
+            exec_command(args)
+            os.write(2, ("Child:    Error: Could not exec %s\n" % args[0]).encode())
+            sys.exit(1)
+        elif '|' in args:
+            os.close(1)                 # redirect child's stdout
+            os.dup(pw)
+            for fd in (pr, pw):
+                print(fd)
+                os.close(fd)
+            print("hello from child")
+        elif '/' in args[0]:
+            try:
+                os.execve(args[0], args, os.environ)
+            except FileNotFoundError:
+                pass
+        else:
+            exec_command(args)
 
     else:                           # parent (forked ok)
-        childPidCode = os.wait()
-        if childPidCode[1] % 256 != 0:
-            os.write(1, ("Program terminated with exit code %d\n" % 
-                childPidCode[1]).encode())
+        if '|' in args:
+            os.close(0)
+            os.dup(pr)
+            for fd in (pw, pr):
+                os.close(fd)
+            for line in fileinput.input():
+                print("From child: <%s>" % line)
+        else:
+            childPidCode = os.wait()
+            if childPidCode[1] % 256 != 0:
+                os.write(1, ("Program terminated with exit code %d\n" % 
+                    childPidCode[1]).encode())
 
 while True:
     pid = os.getpid()
@@ -72,7 +89,8 @@ while True:
     prompt_string = get_ps1()
     
     args = re.split(' ', input(prompt_string).strip())
-    if not args:
+    # make sure to continue prompt in empty string
+    if args[0] == '':
         continue
 
     exec_builtins(args)
