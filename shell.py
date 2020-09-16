e341b50c3646080b2d2287137fe04d57d0303f77
eretana238 <eretana238@gmail.com>
2020-09-15 20:58:19 -0700

    finished pipes and shell

--- shell.py
+++ shell.py
@@ -1,125 +1,127 @@
 #! /usr/bin/env python3
 
-import os, sys, re, fileinput
+import os, re, sys
 
-def has_redirects(user_input):
-    return '>' in user_input or '<' in user_input
+def get_ps1():
+    if 'PS1' in os.environ:
+        return os.environ['PS1']
+    return "\033[1;34;40m %s\x1b[0m$ " % os.getcwd()
 
-def has_pipes(user_input):
-    return '|' in user_input
-
-# checks and executes builtin commands "cd" and "exit"
-def exec_builtin(command):
-    if command[0] not in builtin_cmd:
-        return 
+def exec_builtins(command):
+    global wait
     if command[0] == 'exit':
         sys.exit(0)
+    elif '&' in command:
+        wait = False
+        command.remove('&')
     elif command[0] == 'cd': 
-        cwd = os.getcwd()
         if len(command) > 1:
             try: 
-                os.chdir(os.path.join(cwd,command[1]))
+                os.chdir(command[1])
             except FileNotFoundError: 
-                print('-bash: cd: %s: No such file or directory' % command[1])
+                print('-bash: cd: %s: No such directory' % command[1])
+            except NotADirectoryError:
+                print('-bash: cd: %s: No such directory' % command[1])
 
-def exec_command(commands):
+# checks and executes builtin commands "cd" and "exit"
+def exec_command(command):
     for dir in re.split(":", os.environ['PATH']): 
-        program = "%s/%s" % (dir, commands[0])
+        program = "%s/%s" % (dir, command[0])
         try:
-            os.execve(program, commands, os.environ) 
+            os.execve(program, command, os.environ)
         except FileNotFoundError:
             pass
 
-# parese redirect provided by os instructors
-def parse_redirect(user_input):
-    output_file = None
-    input_file = None
-    cmd = ''
- 
-    if '>' in user_input:
-        [cmd, output_file] = user_input.split('>',1)
-        output_file = output_file.strip()
- 
-    elif '<' in cmd:
-        [cmd, input_file] = cmd.split('<', 1)
-        input_file = input_file.strip()
-    
-    elif output_file != None and '<' in output_file:
-        [output_file, input_file] = output_file.split('<', 1)
-        
-        output_file = output_file.strip()
-        input_file = input_file.strip()
-        
-    return cmd.split(), output_file, input_file
+def exec_pipe(args):
+    cmd1 = args[:args.index('|')]
+    cmd2 = args[args.index('|')+1:]
+    print(cmd2)
 
-def parse_pipe(user_input):
-    cmd1, cmd2 = None
-    if '|' in user_input:
-        [cmd1, cmd2] = user_input.split('|',1)
-    return cmd1, cmd2
+    pr,pw = os.pipe()
+    for f in (pr, pw):
+        os.set_inheritable(f, True)
 
+    rc = os.fork()
+    if rc < 0:
+        os.write(2, ("fork failed, returning %d\n" % rc).encode())
+        sys.exit(1)
+    elif rc == 0:
+        os.close(1)                 # redirect child's stdout
+        os.dup(pw)
+        os.set_inheritable(1, True)
+        for fd in (pr, pw):
+            os.close(fd)
+        exec_command(cmd1)
+        sys.exit(1)
+    else:
+        os.close(0)
+        os.dup(pr)
+        os.set_inheritable(0, True)
+        for fd in (pw, pr):
+            os.close(fd)
+        if '|' in cmd2:
+            exec_pipe(cmd2)
+            sys.exit(1)
+        exec_command(cmd2)
 
-pid = os.getpid()               # get and remember pid
+def redirect(args,n):
+    os.close(n)                 # redirect child's stdout
+    if n == 0:
+        os.open(args[-1], os.O_RDONLY);
+    else:
+        os.open(args[-1], os.O_CREAT | os.O_WRONLY);
+    os.set_inheritable(n, True)
 
-builtin_cmd = ['cd','exit']
+def run_process(args):
+    rc = os.fork()
 
-# check if PS1 exists
-prompt_string = '$ '
-if 'PS1' in os.environ:
-    prompt_string = os.environ['PS1']
-if __name__ == "__main__":
-    while True:
-        user_input = input(prompt_string)
-        if not user_input:
-            continue
+    if rc < 0:
+        os.write(2, ("fork failed, returning %d\n" % rc).encode())
+        sys.exit(1)
 
-        args = re.split('\s', user_input)
-        exec_builtin(args)
-
-        pr = -1
-        pw = -1
-
-        rc = os.fork()
-
-        if rc < 0:
-            os.write(2, ("fork failed, returning %d\n" % rc).encode())
+    elif rc == 0:                   # child
+        if '>' in args:
+            redirect(args,1)
+            exec_command(args)
+            os.write(2, ("Child:    Error: Could not exec %s\n" % args[0]).encode())
+            sys.exit(1)
+        elif '<' in args:
+            redirect(args,0)
+            exec_command(args)
+            os.write(2, ("Child:    Error: Could not exec %s\n" % args[0]).encode())
+            sys.exit(1)
+        elif '|' in args:
+            exec_pipe(args)
+            sys.exit(1)
+        elif '/' in args[0]:
+            try:
+                os.execve(args[0], args, os.environ)
+            except FileNotFoundError:
+                pass
+        else:
+            exec_command(args)
             sys.exit(1)
 
-        elif rc == 0:                   # child
-            if has_redirects(user_input):
-                redirect_args = parse_redirect(user_input)
-                if redirect_args[1]:
-                    os.close(1)                 # redirect child's stdout
-                    os.open(redirect_args[1], os.O_CREAT | os.O_WRONLY);
-                else:
-                    os.close(0)
-                    os.open(redirect_args[2], os.O_CREAT | os.O_WRONLY);
-                
-                os.set_inheritable(1, True)
-                exec_command(list(redirect_args[0]))
-                sys.exit(1)
-            if has_pipes(user_input):
-                pr,pw = os.pipe()
-                for f in (pr, pw):
-                    os.set_inheritable(f, True)
-                os.close(1)                 # redirect child's stdout
-                os.dup(pw)
-                for fd in (pr, pw):
-                    print(fd)
-                    os.close(fd)
-                print('piped')
-            else:
-                exec_command(args)
-                sys.exit(1)
-        else:                           # parent (forked ok)
-            if has_pipes(user_input):
-                os.close(0)
-                os.dup(pr)
-                for fd in (pw, pr):
-                    os.close(fd)
-                for line in fileinput.input():
-                    print("From child: <%s>" % line)
-            else:
-                childPidCode = os.wait()
-                if childPidCode[1] % 256 != 0:
-                    os.write(1, ("Program terminated with exit code %d\n" % childPidCode[1]).encode())
+    else:                           # parent (forked ok)
+        if wait:
+            childPidCode = os.wait()
+            if childPidCode[1] % 256 != 0:
+                os.write(1, ("Program terminated with exit code %d\n" % 
+                    childPidCode[1]).encode())
+
+while True:
+    wait = True
+    pid = os.getpid()
+
+    prompt_string = get_ps1()
+    
+    args = re.split(' ', input(prompt_string).strip())
+    # make sure to continue prompt in empty string
+    if args[0] == '':
+        continue
+
+    exec_builtins(args)
+
+    run_process(args)
+
+    
